2187690f749c12dd008f6eb767a4529630a51ffb
jpaberzs <janis.paberzs18@gmail.com>
2024-10-20 17:46:06 +0300

    refactor: DELETE request

---
+++ src/controllers/delete.ts
@@ -0,0 +1,15 @@
+import { ServerResponse } from 'http';
+import { deleteUser } from '../services/userService';
+
+export const DELETE = (res: ServerResponse, id: string | null) => {
+  if (id) {
+    const isDeleted = deleteUser(id);
+    if (isDeleted) {
+      res.writeHead(204);
+      res.end();
+    } else {
+      res.writeHead(404, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: 'User not found' }));
+    }
+  }
+};
